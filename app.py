9e16a80f8bde1e510814d29e8bba715c76d94b31
Naoshi Watanuki <kosei-halfmoon4583@users.noreply.github.com>
2023-04-20 15:04:53 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+
+import streamlit as st
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": "あなたはPGI Support-AIです。"}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )  
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title("Primagest, Inc.")
+st.write("PGI Support BOT (_Powered by ChatGPT(GPT-3.5_turbo")
+
+user_input = st.text_input("質問、コメントなどをどうぞ。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "🤓"
+        if message["role"]=="assistant":
+            speaker="👽"
+
+        st.write(speaker + ": " + message["content"])
